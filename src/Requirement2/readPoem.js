e44d2b507a335c9ca1571b996b2261aef0830559
girishnasre246 <girishnasre246@gmail.com>
2024-04-20 22:20:55 +0530

    Updated Requirement 2

--- src/Requirement2/readPoem.js
+++ src/Requirement2/readPoem.js
@@ -1,11 +1,22 @@
-import { readFile } from 'fs/promises';
+import fs from 'fs';
 
+function readFileWithCallback(callback) {
+    fs.readFile('poem.txt', 'utf8', (err, data) => {
+        if (err) {
+            callback(err, null);
+            return;
+        }
 
-readFile('poem.txt', 'utf8')
-  .then(data => {
+        callback(null, data);
+    });
+}
+
+readFileWithCallback((err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+
     console.log('Contents of the poem:');
     console.log(data);
-  })
-  .catch(err => {
-    console.error('Error reading the file:', err);
-  });
+});
